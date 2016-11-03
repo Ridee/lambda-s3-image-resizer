90cdf515511c5725483515a33632031884599682
Alex <alex@jinnapp.com>
2016-11-03 14:44:21 +0000

    Changes to please Anderdog

--- lambda-s3-image-resizer.js
+++ lambda-s3-image-resizer.js
@@ -63,9 +63,9 @@ exports.handler = function (event, context, callback) {
       function checkFormat(response, next) {
         console.log('Finished donwloading image');
         console.log('Checking image format');
-	      formatStr = "Image format is";
+        var formatStr = "Image format is";
         formatStr = format.concat(gm(response.Body).format);
-	      console.log(formatStr);
+        console.log(formatStr);
         if (gm(response.Body).format === 'jpeg') {
           next(null, response);
         } else {
